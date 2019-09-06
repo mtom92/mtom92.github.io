752933d0ae49a1b054f496f165fbfc2ee7ca33e6
mtom92 <torresmoranm@gmail.com>
2019-09-06 10:54:55 -0700

    Updates

--- precache-manifest.4b773ab6526cfca7768680f27adc46e1.js
+++ precache-manifest.4b773ab6526cfca7768680f27adc46e1.js
@@ -1,6 +1,6 @@
 self.__precacheManifest = (self.__precacheManifest || []).concat([
   {
-    "revision": "1ff995c0966bf15b2cbb2fcbcadf1307",
+    "revision": "c650c541d75d9ded5b5180f84238a4cd",
     "url": "mtom92.github.io/index.html"
   },
   {
@@ -8,7 +8,7 @@ self.__precacheManifest = (self.__precacheManifest || []).concat([
     "url": "mtom92.github.io/static/css/2.266e55a5.chunk.css"
   },
   {
-    "revision": "80c8d00977a9cdaf9205",
+    "revision": "607bb6b14d2c81db1e4b",
     "url": "mtom92.github.io/static/css/main.b0391421.chunk.css"
   },
   {
@@ -16,8 +16,8 @@ self.__precacheManifest = (self.__precacheManifest || []).concat([
     "url": "mtom92.github.io/static/js/2.dca7acc9.chunk.js"
   },
   {
-    "revision": "80c8d00977a9cdaf9205",
-    "url": "mtom92.github.io/static/js/main.e0415124.chunk.js"
+    "revision": "607bb6b14d2c81db1e4b",
+    "url": "mtom92.github.io/static/js/main.cde781be.chunk.js"
   },
   {
     "revision": "d3d18ac1993ab29a164c",
