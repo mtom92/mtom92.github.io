fb5afea550d785c1b714115d7bc034d70b245ca1
mtom92 <torresmoranm@gmail.com>
2019-09-06 11:43:59 -0700

    changed resume  url

--- src/sections/Contact.js
+++ src/sections/Contact.js
@@ -23,7 +23,7 @@ class Contact extends Component {
            <a href="https://github.com/mtom92"><FaGithubAlt size={42} color='#2eabb2' id="icon"/></a>
            <a href="https://www.linkedin.com/in/gerardo-miguel-torres-moran/"><FaLinkedin size={42} color='#2eabb2' id="icon"/></a>
            <a href="mailto:torresmoranm@gmail.com"><FaRegEnvelope  size={42} color='#2eabb2' id="icon"/></a>
-           <a href="https://mtom92.github.io/mtom92.github.io/static/media/mt_resume.pdf"><FaRegFilePdf  size={40} color='#2eabb2' id="icon"/></a>
+           <a href="https://drive.google.com/file/d/11NUUhq0CY0UQ3SPSP5Im4mYGSch_ft6p/view?usp=sharing"><FaRegFilePdf  size={40} color='#2eabb2' id="icon"/></a>
          </Col>
        </Row>
      </Container>
